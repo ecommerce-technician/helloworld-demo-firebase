b78c60b99d222d793e393be5fa052c540c70a812
ecommerce-technician <aw2.alexwood@gmail.com>
2016-09-26 11:37:48 -0700

    adds link for admin request

--- assets/NodeTechApp/services/Auth.js
+++ assets/NodeTechApp/services/Auth.js
@@ -2,14 +2,14 @@ angular.module('NodeTechApp')
     .factory("Users", function ($http) {
         usersObj = {};
         usersObj.getUsers = function() {
-            return $http.get('/api/v1/users')
+            return $http.get('http://104.236.23.95:3100/api/v1/users')
         }
         return usersObj;
     })
     .factory("User", function ($http) {
         userObj = {};
         userObj.getLoggedIn = function() {
-            return $http.get('/api/v1/user')
+            return $http.get('http://104.236.23.95:3100/api/v1/user')
         }
         return userObj;
     })
